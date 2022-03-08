22894a195cc6fabbfe73196cd117d498bc608618
Bernardo Lima <bernardomonclaro@gmail.com>
2022-03-08 15:07:51 -0300

    subindo pro git

---
+++ app/models/contact.rb
@@ -0,0 +1,13 @@
+class Contact < ApplicationRecord
+  belongs_to :kind #, optional: true
+  has_many :phones
+  has_one :address
+
+  accepts_nested_attributes_for :phones, allow_destroy: true
+  accepts_nested_attributes_for :address, update_only: true
+  def as_json(options = {})
+    h = super(options)
+    h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
+    h
+  end
+end
